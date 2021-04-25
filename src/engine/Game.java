a476332068f5fc4dc6b1105d4fb55b64e16d8b5c
Mohamed Medhat <mmedhat1910@gmail.com>
2021-04-26 00:51:50 +0200

    Added CSV Funtions 1

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,7 +1,7 @@
 package engine;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Game {
 	private Player player;
@@ -12,7 +12,9 @@ public class Game {
 	
 	public Game(String playerName,String playerCity) throws IOException {
 		player = new Player(playerName);
+		this.distances = new ArrayList<Distance>();
 		this.currentTurnCount = 1;
+		this.loadCitiesAndDistances();
 	}
 	
 	
@@ -40,6 +42,58 @@ public class Game {
 	public void setCurrentTurnCount(int currentTurnCount) {
 		this.currentTurnCount = currentTurnCount;
 	}
+	
+	public static  ArrayList<String> readCSV (String path) throws IOException {
+		ArrayList<String> list = new ArrayList<String>();
+		String currentLine = "";
+		FileReader fileReader = new FileReader(path);
+		BufferedReader br = new BufferedReader(fileReader);
+		while((currentLine = br.readLine()) != null) {
+			list.add(currentLine);
+		}
+		return list;
+	}
+	
+	public static void loadArmy(String cityName, String path) throws IOException {
+		
+	}
+	private void loadCitiesAndDistances() throws IOException {
+		String currentLine = "";
+		Set<String> set = new HashSet<String>();
+		FileReader fileReader = new FileReader("CSV_Files/distances.csv");
+		BufferedReader br = new BufferedReader(fileReader);
+		while((currentLine = br.readLine()) != null) {
+			String[] city = currentLine.split(",");
+			
+//			Load Cities
+		
+			
+			
+			
+			
+			
+//			Load Distances
+			Distance d = new Distance(city[0], city[1], Integer.parseInt(city[2]));
+			this.distances.add(d);
+			
+			
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		Game g = new Game("Mohamed", "Cairo");
+		for(int i = 0;i<g.getDistances().size();i++) {
+			System.out.print(g.getDistances().get(i).getFrom()+ " ");
+			System.out.print(g.getDistances().get(i).getTo() + " \n");
+		}
+	}
+	
+	
+	
+	
+	
 	
 	
 }
